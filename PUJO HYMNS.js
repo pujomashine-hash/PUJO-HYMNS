3f3743c0a041f01a2b57462c34ba5be9196cec19
pujomashine-hash <pujomashine@gmail.com>
2026-03-17 10:03:36 +0300

    Update PUJO HYMNS.js

--- PUJO HYMNS.js
+++ PUJO HYMNS.js
@@ -1,8 +1,7 @@
-
+       
 const songbtns=document.querySelectorAll(".nyimbo")
 const songlist=document.getElementById("song-list")
 const songdetails=document.getElementById("song-details")
-const title=document.getElementById("title")
 const lyrics=document.getElementById("lyrics")
 const play=document.getElementById("play")
 const pause=document.getElementById("pause")
@@ -11,11 +10,8 @@ const audio=document.getElementById("audio")
 
 songbtns.forEach(btn => {
     btn.addEventListener("click",()=> {
-        const songtitle=btn.dataset.title;
         const lyricsFile=btn.dataset.lyrics;
         const filekey=btn.dataset.file;
-
-        title.textcontent=songtitle; //kwa title tu
 
         fetch(lyricsFile)
             .then(res => res.text())
@@ -27,45 +23,46 @@ songbtns.forEach(btn => {
          .then(data => {
            audio.src=data[filekey]; //hii ni kwa audio
          });
-
-      
-
       songlist.style.display="none";
       songdetails.style.display="block";
     });
 });
-
 back.addEventListener("click", function() {
     songlist.style.display="block";
     songdetails.style.display="none";
     audio.pause();
 });
-
 play.addEventListener("click",function() {
     audio.play();
 });
-
 pause.addEventListener('click',function() {
     audio.pause();
 });
-
+    //SIMPLE SEARCH BARx
 const searchInput = document.getElementById("search");
 const songs = document.querySelectorAll(".nyimbo");
-
 searchInput.addEventListener("keyup", function () {
-
   const searchValue = searchInput.value.toLowerCase();
-
-  songs.forEach(function (song) {
-
-    const title = song.textContent.toLowerCase();
-
-    if (title.includes(searchValue)) {
+ songs.forEach(function (song) {
+   const title = song.textContent.toLowerCase();
+      if (title.includes(searchValue)) {
       song.style.display = "";
     } else {
       song.style.display = "none";
     }
-
   });
-
 });
+    //NAVIGATION BAR
+  const Buttons=document.querySelectorAll(".change")
+  const screens=document.querySelectorAll(".screen")
+   Buttons.forEach(Btn => {
+     Btn.addEventListener('click',()=> {
+       Buttons.forEach(B => B.classList.remove('active'))
+       Btn.classList.add('active')
+  screens.forEach(screen => screen.style.display="none");
+  const targetId= Btn.getAttribute("data-target")
+  document.getElementById(targetId).style.display="block";
+     });
+   });
+     
+   
