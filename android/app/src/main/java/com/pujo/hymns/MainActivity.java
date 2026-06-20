5d22dfb09e6c7fec55982ffccb018964b93e85d1
pujomashine-hash <pujomashine@gmail.com>
2026-06-20 18:31:35 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/pujo/hymns/MainActivity.java
+++ android/app/src/main/java/com/pujo/hymns/MainActivity.java
@@ -1,5 +1,13 @@
 package com.pujo.hymns;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import io.github.jofr.capacitor.mediasessionplugin.MediaSessionPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    registerPlugin(MediaSessionPlugin.class);
+  }
+}
