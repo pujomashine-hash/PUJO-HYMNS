9bfc3b10ee29047f98483c9a434fa8919ec02cb3
pujomashine-hash <pujomashine@gmail.com>
2026-06-20 18:04:06 +0300

    Create Player.js

---
+++ www/Player.js
@@ -0,0 +1,36 @@
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  
+const progress = document.getElementById("progress");
+const audio = document.getElementById("audio");
+if(audio){
+audio.addEventListener("timeupdate", () => {
+  if (audio.duration) {
+    const percent = (audio.currentTime / audio.duration) * 100;
+    progress.style.width = percent + "%";
+  }
+});
+}
+
+
+
+//Seek when user clicks on progress bar
+const progressContainer=document.getElementById("progress-container")
+if(progressContainer){
+progressContainer.addEventListener("click", (e) => {
+  const rect = progressContainer.getBoundingClientRect();
+  const clickX = e.clientX - rect.left;
+  const width = rect.width;
+  const percent = clickX / width;
+  audio.currentTime = percent * audio.duration;
+});
+}
+if(audio){
+audio.addEventListener("error", () => {
+  playing.textContent = "❌ Audio not available";
+  Downloadbtn.textContent="❔"
+});
+}
+
+});
