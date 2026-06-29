8ed878c1d669425b4d6259cfe7e1332b09dc77da
pujomashine-hash <pujomashine@gmail.com>
2026-06-29 08:15:35 +0300

    Update Playlist-screen.js

--- www/Playlist-screen.js
+++ www/Playlist-screen.js
@@ -1,191 +1,138 @@
-// NEW (FAVOURITES)
-      const Filesystem = window.Capacitor?.Plugins?.Filesystem;
-const MediaSession = window.Capacitor?.Plugins?.MediaSession;
+document.addEventListener("DOMContentLoaded", () => {
+  
+  window.initPlaylistScreen = function () {
 
-const favBtn = document.getElementById("fav");
-let favourites = JSON.parse(localStorage.getItem("favourites")) || [];
-
-// ❤️ FAVOURITE TOGGLE
-if (favBtn) {
-  favBtn.addEventListener("click", () => {
-    if (!window.currentSong) return;
-
-    const exists = favourites.some(
-      song => song.title === currentSong.title
-    );
-
-    if (exists) {
-      favourites = favourites.filter(
-        s => s.title !== currentSong.title
-      );
-    } else {
-      favourites.push({
-  title: currentSong.title,
-  artist: currentSong.artist,
-  file: currentSong.file,
-  lyrics: currentSong.lyrics,
-  image: currentSong.image
-});
-    }
-
-    localStorage.setItem(
-      "favourites",
-      JSON.stringify(favourites)
-    );
-
-    updateFavButton();
-    renderFavourites()
+    const CategoryNames = document.getElementById("Category-names");
+    const CategorySongs = document.getElementById("Category-songs");
+    const Jina = document.getElementById("Catjina");
+    const Exitbtn = document.getElementById("Exit");
     
-  });
-}
-
-// UPDATE FAV BUTTON
-function updateFavButton() {
-  if (!window.currentSong) return;
-
-  const exists = favourites.some(
-    song => song.title === currentSong.title
-  );
-
-  favBtn.textContent = exists ? "❤️" : "♡";
-}
-// FAVOURITE SCREEN
-const favScreen = document.getElementById("favourite");
-
-function renderFavourites() {
-  if (!favScreen) return;
-
-  favScreen.innerHTML = "";
-
-  if (favourites.length === 0) {
-    favScreen.innerHTML = `
-      <h1 class="favmessage">Your favourite Songs</h1>
-      <p class="favmessage">No favourite songs yet</p>
-    `;
+    function buildCategorySongs() {
+  if (CategorySongs.querySelector(".nyimbo")) return; // tayari zipo
+  if (!window.allSongs) {
+    setTimeout(buildCategorySongs, 200); // jaribu tena baada ya 200ms
     return;
   }
 
-  favourites.forEach(song => {
-
+  window.allSongs.forEach(song => {
     const btn = document.createElement("button");
     btn.className = "nyimbo";
+    btn.dataset.file = song.file;
+    btn.dataset.lyrics = song.lyrics;
+    btn.dataset.Category = song.Category;
+    btn.dataset.image = song.image;
 
     btn.innerHTML = `
       <div class="left-img">
         <div class="btn-image">
-          <img
-            src="${song.image ? song.image : 'defaul.jpg'}"
-            onerror="this.src='logo.png'">
+          <img src="${song.image ? song.image : 'defaul.jpg'}" onerror="this.src='logo.png'">
         </div>
-
         <div class="text-btn">
           <div class="title">${song.title}</div>
           <div class="artist">${song.artist}</div>
         </div>
       </div>
-
       <span class="three-dots">⋮
         <div class="dots-menu">
-          <button class="share">Share</button>
+          <button class="share"> Share </button>
         </div>
       </span>
     `;
-    
 
     
+    btn.style.display = "none";
     btn.addEventListener("click", async () => {
-
-      const songDetails =
-        document.getElementById("song-details");
-
-      const lyrics =
-        document.getElementById("lyrics");
-
-      const audio =
-        document.getElementById("audio");
-
-      const Playing =
-        document.getElementById("playing");
+  const songDetails = document.getElementById("song-details");
+  const lyrics = document.getElementById("lyrics");
+  const audio = document.getElementById("audio");
+  const Playing = document.getElementById("playing");
+  const Filesystem = window.Capacitor?.Plugins?.Filesystem;
 
       window.initMediaControls(audio, play);
+  window.currentSong = {
+    title: song.title,
+    artist: song.artist,
+    file: song.file,
+    lyrics: song.lyrics,
+    image: song.image
+  };
 
-      window.currentSong = song;
       
-     Playing.textContent =
-        currentSong.title +
-        " - " +
-        currentSong.artist;
+  const fileName = song.file.split("/").pop();
 
-      console.log(currentSong);
+  try {
+    await Filesystem.stat({ path: fileName, directory: "DATA" });
+    const result = await Filesystem.readFile({ path: fileName, directory: "DATA" });
+    audio.src = "data:audio/mpeg;base64," + result.data;
+  } catch (e) {
+    audio.src = song.file;
+  }
+
+      
+  Playing.textContent = window.currentSong.title + " - " + window.currentSong.artist;
+
       setTimeout(() => {
         MediaSession?.setMetadata({
   title: window.currentSong.title,
   artist: window.currentSong.artist,
   artwork: []
 });
-       },0);
+ },0);
+
       
-
-      const fileName =
-        song.file.split("/").pop();
-
-      try {
-
-        await Filesystem.stat({
-          path: fileName,
-          directory: "DATA"
-        });
-
-        const result =
-          await Filesystem.readFile({
-            path: fileName,
-            directory: "DATA"
-          });
-
-        audio.src =
-          "data:audio/mpeg;base64," +
-          result.data;
-
-      } catch (e) {
-
-        audio.src = song.file;
-
-      }
-
-      fetch(song.lyrics)
-        .then(res => res.text())
-        .then(text => {
-          lyrics.innerHTML =
-            text.replace(/\n/g, "<br>");
-        });
-
-      // Ficha screens zote
-      document
-        .querySelectorAll(".screen")
-        .forEach(screen => {
-          screen.style.display = "none";
-        });
-
-      // Onyesha details
-      songDetails.style.display = "block";
-
-      // Ficha favourite page
-      favScreen.style.display = "none";
-
-      // Update buttons
-      if (typeof updateDownloadBtn === "function") {
-        updateDownloadBtn();
-      }
-
-      updateFavButton();
-
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
+  fetch(song.lyrics)
+    .then(res => res.text())
+    .then(text => {
+      lyrics.innerHTML = text.replace(/\n/g, "<br>");
     });
 
-    favScreen.appendChild(btn);
+  if (window.updateDownloadBtn) window.updateDownloadBtn();
+  if (window.updateFavButton) window.updateFavButton();
+
+  document.querySelectorAll(".screen").forEach(s => s.style.display = "none");
+  songDetails.style.display = "block";
+});
+    CategorySongs.appendChild(btn);
   });
 }
 
-// INIT
-renderFavourites();
+buildCategorySongs();
+
+    
+    // CATEGORY CLICK
+    document.querySelectorAll(".Category").forEach(Cat => {
+      Cat.addEventListener("click", () => {
+        const Category = Cat.id;
+        window.activeCategory = Category;
+
+        document.querySelectorAll("#Category-songs .nyimbo").forEach(btn => {
+          btn.style.display = btn.dataset.Category === Category ? "block" : "none";
+        });
+
+        Jina.textContent = Cat.textContent;
+        CategorySongs.style.display = "block";
+        window.categoryView = "songs";
+        CategoryNames.style.display = "none";
+        document.getElementById("Catjina-Container").style.display = "block";
+      });
+    });
+
+    // EXIT BUTTON
+    if (Exitbtn) {
+      Exitbtn.addEventListener("click", (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+
+        window.activeCategory = null;
+        window.categoryView = "names";
+
+        CategoryNames.style.display = "grid";
+        CategorySongs.style.display = "none";
+        document.getElementById("Catjina-Container").style.display = "none";
+      });
+    }
+
+  };
+  
+
+});
