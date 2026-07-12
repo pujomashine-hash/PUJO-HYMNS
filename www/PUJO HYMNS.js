488c3fda17aac2c760a499625a180276e7bfd6ce
pujomashine-hash <pujomashine@gmail.com>
2026-07-12 19:08:09 +0300

    Update PUJO HYMNS.js

--- www/PUJO HYMNS.js
+++ www/PUJO HYMNS.js
@@ -234,6 +234,36 @@ const MediaSession = window.Capacitor?.Plugins?.MediaSession;
     const back = document.getElementById("back");
     const categories=document.querySelectorAll(".category")
     const favourite=document.getElementById("favourite")
+// ===== MEDIA SESSION CONTROLS =====
+if (MediaSession && !controlsInitialized) {
+  controlsInitialized = true;
+
+  MediaSession.setActionHandler(
+    { action: "play" },
+    async () => {
+      await audio.play();
+
+      play.textContent = "▶";
+
+      MediaSession.setPlaybackState({
+        playbackState: "playing"
+      });
+    }
+  );
+
+  MediaSession.setActionHandler(
+    { action: "pause" },
+    async () => {
+      audio.pause();
+
+      play.textContent = "⏯";
+
+      MediaSession.setPlaybackState({
+        playbackState: "paused"
+      });
+    }
+  );
+} 
   window.scrollPosition = 0;
    songList.addEventListener("click", async(e) => {
   const btn = e.target.closest(".nyimbo");
@@ -264,15 +294,15 @@ const MediaSession = window.Capacitor?.Plugins?.MediaSession;
     artist: btn.querySelector(".artist").textContent,
     file: btn.dataset.file,
     lyrics: btn.dataset.lyrics,
-    lazima: btn.dataset.image
+    image: btn.dataset.image
   };
 
   MediaSession?.setMetadata({
-  title: currentSong.title,
-  artist: currentSong.artist,
-  artwork: []   // ✅ lazima iwepo, hata kama tupu
+  title: window.currentSong.title,
+  artist: window.currentSong.artist,
+  artwork: []
 });
-
+     
   Playing.textContent = currentSong.title + " - " + currentSong.artist;
   
 
@@ -411,6 +441,7 @@ if (window.Capacitor) {
 
   });
 }
+
 
 const container = document.getElementById("playlist-container");
 
