6fa11152c34c2633b49c8424cfaaffa28d00a174
pujomashine-hash <pujomashine@gmail.com>
2026-09-18 10:50:20 +0300

    Update Search.js

--- www/Search.js
+++ www/Search.js
@@ -1,20 +1,65 @@
-
-// SEARCH 
+document.addEventListener("DOMContentLoaded",()=>{
+const songList = document.getElementById("song-list")
+const searchScreen= document.getElementById("search-screen")
+window.notificationToggle= document.getElementById("notification-toggle") 
+window.searchToggle= document.getElementById("search-toggle") 
 const searchInput = document.getElementById("search");
+const searchResults = document.getElementById("search-results");
 
-if (searchInput) {
-  searchInput.addEventListener("keyup", () => {
-    const searchValue = searchInput.value.toLowerCase();
+searchInput.addEventListener("input", () => {
+  const value = searchInput.value.toLowerCase().trim();
 
-    document.querySelectorAll(".nyimbo").forEach(btn => {
-      const title = btn.querySelector(".title").textContent.toLowerCase();
-      const artist = btn.querySelector(".artist").textContent.toLowerCase();
+  searchResults.innerHTML = "";
 
-      if (title.includes(searchValue) || artist.includes(searchValue)) {
-        btn.style.display = "";
-      } else {
-        btn.style.display = "none";
-      }
-    });
+  if (!value) {
+  searchResults.innerHTML = "";
+  return;
+  }
+ let found = false;
+  window.allSongs.forEach(song => {
+
+    if (
+      song.title.toLowerCase().includes(value) ||
+      song.artist.toLowerCase().includes(value)
+    ) {
+      searchResults.appendChild(createSongButton(song));
+      found=true;
+    }
+
   });
-}
+  window.onlineSongs.forEach(song => {
+  if (
+    song.title.toLowerCase().includes(value) ||
+    song.artist.toLowerCase().includes(value)
+  ) {
+    searchResults.appendChild(createOnlineSongs(song));
+    found=true;
+  }
+});
+  if(found===false) {
+    searchResults.innerHTML=`
+    <div id="not-found">
+     <h4>No matched results </h4>
+     <p> Try another song title or artist </p>
+    </div>`
+  }
+  alert(found)
+});
+
+  
+searchResults.addEventListener("click", (e) => {
+  const btn = e.target.closest(".nyimbo, .online-btn");
+  if (!btn) return;
+
+  openSong(btn);
+  lastScreen="search-screen"
+  searchScreen.style.display="none"
+});
+  searchToggle.addEventListener("click",()=>{
+    searchScreen.style.display="block"
+    songList.style.display="none"
+  })
+  notificationToggle.addEventListener("click",()=>{
+  openPopup("Notice","<h5>No new notification</h5>")
+  })
+})
