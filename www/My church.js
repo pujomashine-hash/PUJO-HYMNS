f082868f6cea515b625f29176f3423bf97e02d7d
pujomashine-hash <pujomashine@gmail.com>
2026-09-19 20:51:14 +0300

    Update My church.js

--- www/My church.js
+++ www/My church.js
@@ -23,7 +23,7 @@ async function getChurchSongs() {
   try {
 
     const res = await fetch(
-      `http://localhost:3000/songs?church=Lutheran%20choir`
+      `https://pujo-server.onrender.com/songs?church=Lutheran%20choir`
     );
 
     const data = await res.json();
